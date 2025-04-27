beb2ae6f6f469c27c368f98410fc95ab7187af68
Iota-02 <agus.munoz10@gmail.com>
2025-04-26 21:41:45 -0700

    Added New Rooms
    Added a 3 door catwalk room, pit long and pit short rooms.

---
+++ Assets/Warehouse Maker/Warehouse Modular/Scripts/WarehouseModular.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+[ExecuteInEditMode]
+
+public class WarehouseModular : MonoBehaviour
+{
+    [HideInInspector]
+    public List<GameObject> itemsList = new List<GameObject>();
+    [HideInInspector]
+    public GameObject largeWall,mediumWall,smallWall,miniWall,
+                      tinyWall,windowWall,smallWindowWall,innerCorner,
+                      outerCorner,garageFrame,doorFrame,doubleDoorFrame;
+    private MeshFilter myMeshFilter;
+
+    private void Start()
+    {
+        myMeshFilter = GetComponent<MeshFilter>();
+        largeWall = Resources.Load("Models/LargeWall")as GameObject;
+        mediumWall = Resources.Load("Models/MediumWall") as GameObject;
+        smallWall = Resources.Load("Models/SmallWall") as GameObject;
+        miniWall = Resources.Load("Models/Extra_SmallWall") as GameObject;
+        tinyWall = Resources.Load("Models/Extra_SmallWall1") as GameObject;
+        windowWall = Resources.Load("Models/WindowWall") as GameObject;
+        smallWindowWall = Resources.Load("Models/SmallWindowWall") as GameObject;
+        innerCorner = Resources.Load("Models/LeftCorner") as GameObject;
+        outerCorner = Resources.Load("Models/RightCorner") as GameObject;
+        garageFrame = Resources.Load("Models/GarageDoorFrame") as GameObject;
+        doorFrame = Resources.Load("Models/DoorWall") as GameObject;
+        doubleDoorFrame = Resources.Load("Models/DoubleDoorWall") as GameObject;
+    }
+
+    public void BuildNextItem(GameObject item)
+    {
+        if (itemsList.Count == 0)
+        {
+            GameObject first = Instantiate(item, transform.position, item.transform.rotation);
+            first.transform.SetParent(transform);
+            itemsList.Add(first);
+        }
+        else {
+
+           Transform lastItem = itemsList.Last<GameObject>().transform.GetChild(0);
+           GameObject tmp =  Instantiate(item, lastItem.position, lastItem.rotation);
+           tmp.transform.SetParent(transform);
+           itemsList.Add(tmp);
+        }
+
+    }
+
+    public void DeleteLastItem()
+    {
+        GameObject lastItem = itemsList.Last<GameObject>();
+        if(Application.isPlaying)
+            Destroy(lastItem);
+        if (Application.isEditor)
+            DestroyImmediate(lastItem);
+        itemsList.Remove(lastItem);
+    }
+
+
+}
---
+++ Assets/Warehouse Maker/Warehouse Modular/Scripts/WarehouseModularEditor.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+#if UNITY_EDITOR
+
+
+[CustomEditor(typeof(WarehouseModular))]
+public class WarehouseModularEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var style = new GUIStyle(GUI.skin.button);
+        style.fontSize = 14;
+        style.normal.textColor = Color.yellow;
+
+        DrawDefaultInspector();
+        GUILayout.Label("Walls & Corners");
+        WarehouseModular warehouseModular = (WarehouseModular)target;
+        if(GUILayout.Button("Large Wall"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.largeWall);
+        }
+        if (GUILayout.Button("Medium Wall"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.mediumWall);
+        }
+        if (GUILayout.Button("small Wall"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.smallWall);
+        }
+        if (GUILayout.Button("Mini Wall"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.miniWall);
+        }
+
+        if (GUILayout.Button("Tiny Wall"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.tinyWall);
+        } 
+
+        if (GUILayout.Button("Left Corner"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.innerCorner);
+        }
+        if (GUILayout.Button("Right Corner"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.outerCorner);
+        }
+
+        GUILayout.Label("Doors & Windows");
+        if (GUILayout.Button("Garage Frame"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.garageFrame);
+        }
+
+        if (GUILayout.Button("Door "))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.doorFrame);
+        }
+        if (GUILayout.Button("Double Door"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.doubleDoorFrame);
+        }
+
+        if (GUILayout.Button("Window"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.windowWall);
+        }
+
+        if (GUILayout.Button("Small Window"))
+        {
+            warehouseModular.BuildNextItem(warehouseModular.smallWindowWall);
+        }
+
+        GUILayout.Label("Destroy");
+
+        if (GUILayout.Button("Delete Last Part",style))
+
+        {
+            if(warehouseModular.itemsList.Count > 0)
+                warehouseModular.DeleteLastItem();
+        }
+
+    }
+
+
+}
+#endif
