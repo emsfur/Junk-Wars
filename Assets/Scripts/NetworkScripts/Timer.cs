3f4e45148eb556d0f24c7a141ba35d303e5824e2
al-han <alexhan168@gmail.com>
2025-05-15 01:02:54 -0700

    Timer
    Time should work and synch with host and client

---
+++ Assets/Scripts/NetworkScripts/Timer.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using TMPro;
+using Unity.Netcode;
+
+public class Timer : NetworkBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float startingTime = 300f;
+
+    private NetworkVariable<float> remainingTime = new NetworkVariable<float>(
+        writePerm: NetworkVariableWritePermission.Server);
+
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log("The StartingTime:" + startingTime);
+        if (IsServer)
+        {
+            //remainingTime.Value = startingTime;
+            remainingTime.Value = startingTime;
+            Debug.Log("[Server] Starting Time Set: " + remainingTime.Value);
+        }
+    }
+    // private void Start()
+    // {
+        
+    // }
+
+    private void Update()
+    {
+        Debug.Log("Current Remaining Time: " + remainingTime.Value);
+
+        if (IsServer)
+        {
+            if (remainingTime.Value > 0f)
+            {
+
+                remainingTime.Value -= Time.deltaTime;
+                //remainingTime.Value -= 1;
+            }
+            else
+            {
+                remainingTime.Value = 0f;
+                Debug.Log("[Server] Timer ended.");
+            }
+        }
+        Debug.Log("Update Here" + remainingTime.Value);
+        UpdateTimerDisplay(remainingTime.Value);
+    }
+
+    private void UpdateTimerDisplay(float time)
+    {
+        if (timerText == null)
+        {
+            Debug.LogWarning("TimerText reference is missing!");
+            return;
+        }
+
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        timerText.text = $"{minutes:00}:{seconds:00}";
+    }
+}
